d8875168a5570d16b91f8d60fbede5448937a68f
Jeremiah Akinsowon <akinakinsowon@gmail.com>
2026-01-10 09:49:15 +0100

    Implementing SafeAreas, Images and Touchable Components in React Native

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -1,54 +1,63 @@
-import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 import {
-  View,
   Text,
+  View,
   StyleSheet,
   Image,
   ImageBackground,
   Dimensions,
   TouchableOpacity,
-} from 'react-native';
+} from "react-native";
+
+import {
+  SafeAreaView,
+  SafeAreaProvider,
+} from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        {/* Everything goes inside */}
         <ImageBackground
-          source={require('@/assets/images/background-image.png')}
+          source={require("@/assets/images/background-image.png")}
           style={styles.background}
-          resizeMode='cover'
-          height={Dimensions.get('window').height}
+          resizeMode="cover"
         >
           <View style={styles.container}>
+            {/* Company Logo */}
             <View style={styles.companyLogo}>
-              <Image source={require('@/assets/images/Logo.png')} />
+              <Image source={require("@/assets/images/Logo.png")} />
             </View>
 
+            {/* Text Group */}
             <View style={styles.textGroup}>
               <Text style={styles.textLarge}>
                 Find your favorite place here
               </Text>
-              <Text style={styles.textSmall}>The best prices for over 2</Text>
-              <Text style={styles.textSmall}>million properties worldwide</Text>
-            </View>
-          </View>
-
-          <View style={{ position: 'absolute', bottom: 0, width: '100%' }}>
-            <View style={styles.buttonGroup}>
-              <TouchableOpacity style={styles.button}>
-                <Text style={{ ...styles.textSmall, color: 'black' }}>
-                  Join here
-                </Text>
-              </TouchableOpacity>
-
-              <TouchableOpacity style={styles.transparentButton}>
-                <Text style={styles.textSmall}>Sign In</Text>
-              </TouchableOpacity>
+              <Text style={styles.textSmall}>
+                The best prices for over 2
+              </Text>
+              <Text style={styles.textSmall}>
+                million properties worldwide
+              </Text>
             </View>
 
-            <View style={{ alignItems: 'center', paddingVertical: 20 }}>
-              <Text style={{ color: 'white' }}>Continue to home</Text>
+            {/* Buttons + Bottom Text */}
+            <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
+              <View style={styles.buttonGroup}>
+                <TouchableOpacity style={styles.button}>
+                  <Text style={{ ...styles.textSmall, color: "black" }}>
+                    Join here
+                  </Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.transparentButton}>
+                  <Text style={styles.textSmall}>Sign In</Text>
+                </TouchableOpacity>
+              </View>
+
+              <View style={{ alignItems: "center", paddingVertical: 20 }}>
+                <Text style={{ color: "white" }}>Continue to home</Text>
+              </View>
             </View>
           </View>
         </ImageBackground>
@@ -63,55 +72,55 @@ const styles = StyleSheet.create({
   },
   background: {
     flex: 1,
-    justifyContent: 'center',
-    width: '100%',
-    height: Dimensions.get('window').height,
+    justifyContent: "center",
+    width: "100%",
+    height: Dimensions.get("window").height,
   },
   companyLogo: {
-    width: '100%',
-    alignItems: 'center',
+    width: "100%",
+    alignItems: "center",
     padding: 20,
     marginBottom: 50,
   },
   textGroup: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   textLarge: {
-    color: 'white',
-    fontWeight: '800',
+    color: "white",
+    fontWeight: "800",
     fontSize: 40,
-    textAlign: 'center',
+    textAlign: "center",
     marginBottom: 12,
   },
   textSmall: {
-    color: 'white',
+    color: "white",
     fontSize: 18,
-    fontWeight: '200',
-    textAlign: 'center',
+    fontWeight: "200",
+    textAlign: "center",
   },
   transparentButton: {
-    borderColor: 'white',
+    borderColor: "white",
     borderWidth: 2,
     borderRadius: 40,
     paddingVertical: 15,
     paddingHorizontal: 5,
-    alignItems: 'center',
+    alignItems: "center",
     fontSize: 20,
     flex: 1,
   },
   button: {
-    borderColor: 'white',
+    borderColor: "white",
     borderWidth: 2,
     borderRadius: 40,
     paddingVertical: 15,
     paddingHorizontal: 5,
-    alignItems: 'center',
+    alignItems: "center",
     fontSize: 20,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     flex: 1,
   },
   buttonGroup: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 20,
     paddingHorizontal: 20,
   },
